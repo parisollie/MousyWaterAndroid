6b5640b7088352f43c3e2d9216c6ecc29da5bf9c
parisollie <pjff18@gmail.com>
2023-12-13 00:43:51 -0600

    Button

---
+++ app/src/main/java/com/pjff/mousywater/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.pjff.mousywater.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pjff.mousywater.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
