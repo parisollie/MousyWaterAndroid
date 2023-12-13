e9abdc2612c43974ff4e95bf3982fe7d337d39a0
parisollie <pjff18@gmail.com>
2023-12-13 02:59:21 -0600

    Login and Register

--- app/src/main/java/com/pjff/mousywater/activities/RegisterActivity.kt
+++ app/src/main/java/com/pjff/mousywater/activities/RegisterActivity.kt
@@ -5,16 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import com.pjff.mousywater.R
-import com.pjff.mousywater.databinding.ActivityLoginBinding
+import com.pjff.mousywater.databinding.ActivityRegisterBinding
 
 
-
-/**
- * Login Screen of the application.
- */
+// TODO Step 1: Create an RegisterActivity of the application.
+// START
 @Suppress("DEPRECATION")
-class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
 
     /**
      * This function is auto created by Android when the Activity Class is created.
@@ -23,24 +21,26 @@ class LoginActivity : AppCompatActivity() {
         //This call the parent constructor
         super.onCreate(savedInstanceState)
         // This is used to align the xml view to this class
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // This is used to hide the status bar and make the login screen as a full screen activity.
+
+        // TODO Step 3: Hide the status bar for the LoginActivity to make it full screen activity.
+        // START
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
         // It is deprecated in the API level 30. I will update you with the alternate solution soon.
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-
-        // TODO Step 7: Assign a onclick event to the register text to launch the register activity.
         // START
-        binding.tvRegister.setOnClickListener {
+        binding.tvLogin.setOnClickListener {
 
             // Launch the register screen when the user clicks on the text.
-            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
         }
         // END
     }
 }
+// END
--- app/src/main/java/com/pjff/mousywater/activities/SplashActivity.kt
+++ app/src/main/java/com/pjff/mousywater/activities/SplashActivity.kt
@@ -7,13 +7,11 @@ import android.content.Intent
 import android.os.Handler
 import com.pjff.mousywater.R
 
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        // TODO Step 6: Add the below code to make the screen as full screen and finish the activity after 2500 milliseconds.
-        // START
 
         // This is used to hide the status bar and make the splash screen as a full screen activity.
         // It is deprecated in the API level 30. I will update you with the alternate solution soon.
@@ -26,24 +24,14 @@ class SplashActivity : AppCompatActivity() {
         // It is deprecated in the API level 30.
         Handler().postDelayed(
             {
-                // Launch the Main Activity
-                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                // TODO Step 5: Launch the Login Activity instead of Main Activity.
+                // START
+                // Launch the Login Activity
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                // END
                 finish() // Call this when your activity is done and should be closed.
             },
             2500
         ) // Here we pass the delay time in milliSeconds after which the splash activity will disappear.
-
-        // TODO Step 2: Use the custom font to show the app name in the splash screen.
-        // This is used to get the file from the assets folder and set it to the title textView.
-        // START
-        // TODO Step 5: Comment the Typeface code here once you have replaced the TextView with custom class in the XML file.
-        // START
-       /* val typeface: Typeface =
-            Typeface.createFromAsset(assets, "Montserrat-Bold.ttf")
-        tv_app_name.typeface = typeface*/
-        // END
-        // END
-
-
     }
 }
