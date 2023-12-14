f874084d60e8f498267667694089d95ee48261af
parisollie <pjff18@gmail.com>
2023-12-14 13:37:06 -0600

    Login and Register

---
+++ app/src/main/java/com/pjff/mousywater/activities/BaseActivity.kt
@@ -0,0 +1,46 @@
+package com.pjff.mousywater.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.pjff.mousywater.R
+
+// TODO Step 3: Create an open class name as BaseActivity and inherits the AppCompatActivity class.
+/**
+ * A base activity class is used to define the functions and members which we will use in all the activities.
+ * It inherits the AppCompatActivity class so in other activity class we will replace the AppCompatActivity with BaseActivity.
+ */
+// START
+open class BaseActivity : AppCompatActivity() {
+
+    // TODO Step 4: Create a function to show the success and error messages in snack bar component.
+    // START
+    /**
+     * A function to show the success and error messages in snack bar component.
+     */
+    fun showErrorSnackBar(message: String, errorMessage: Boolean) {
+        val snackBar =
+            Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+
+        if (errorMessage) {
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity,
+                    R.color.colorSnackBarError
+                )
+            )
+        }else{
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity,
+                    R.color.colorSnackBarSuccess
+                )
+            )
+        }
+        snackBar.show()
+    }
+    // END
+}
+// END
--- app/src/main/java/com/pjff/mousywater/activities/RegisterActivity.kt
+++ app/src/main/java/com/pjff/mousywater/activities/RegisterActivity.kt
@@ -1,19 +1,18 @@
 package com.pjff.mousywater.activities
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import android.view.WindowManager
 import com.pjff.mousywater.R
 import com.pjff.mousywater.databinding.ActivityRegisterBinding
+import android.text.TextUtils
+import android.view.WindowManager
 
 
-// TODO Step 1: Create an RegisterActivity of the application.
-// START
+
+// TODO Step 5: Replace the AppCompatActivity with BaseActivity to use the common function which we have created in the BaseActivity class.
 @Suppress("DEPRECATION")
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
     private lateinit var binding: ActivityRegisterBinding
-
     /**
      * This function is auto created by Android when the Activity Class is created.
      */
@@ -24,23 +23,97 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        // TODO Step 3: Hide the status bar for the LoginActivity to make it full screen activity.
-        // START
         // This is used to hide the status bar and make the splash screen as a full screen activity.
         // It is deprecated in the API level 30. I will update you with the alternate solution soon.
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        // START
-        binding.tvLogin.setOnClickListener {
 
-            // Launch the register screen when the user clicks on the text.
-            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
-            startActivity(intent)
+        // TODO Step 2: Call the function to set up the action bar.
+        // START
+        setupActionBar()
+        // END
+
+        // TODO Step 8: Assign a click event to the register button and call the validate function.
+        // START
+
+        binding.btnRegister.setOnClickListener {
+
+            validateRegisterDetails()
         }
         // END
     }
+
+    // TODO Step 1: Create a function to set up an action bar.
+    // START
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(binding.toolbarRegisterActivity)
+
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+        binding.toolbarRegisterActivity.setNavigationOnClickListener { onBackPressed() }
+
+    }
+
+
+    // END
+
+    // TODO Step 6: Create an function to validate the register account fields.
+    // START
+    /**
+     * A function to validate the entries of a new user.
+     */
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+
+            TextUtils.isEmpty(binding.etFirstName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etLastName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etConfirmPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+
+            binding.etPassword.text.toString().trim { it <= ' ' } != binding.etConfirmPassword.text.toString()
+                .trim { it <= ' ' } -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
+                false
+            }
+            !binding.cbTermsAndCondition.isChecked -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("Your details are valid.", false)
+                true
+            }
+        }
+    }
+    // END
 }
-// END
