25d3ea9caa4b492ff06401302322fdc6c0cfeac0
parisollie <pjff18@gmail.com>
2023-12-14 16:04:19 -0600

    Forget Password

---
+++ app/src/main/java/com/pjff/mousywater/activities/ForgotPasswordActivity.kt
@@ -0,0 +1,90 @@
+package com.pjff.mousywater.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.pjff.mousywater.R
+import com.pjff.mousywater.databinding.ActivityForgotPasswordBinding
+import com.pjff.mousywater.databinding.ActivityLoginBinding
+
+// TODO Step 1: Replace the AppCompactActivity with BaseActivity.
+/**
+ * Forgot Password Screen of the application.
+ */
+class ForgotPasswordActivity : BaseActivity() {
+    private lateinit var binding: ActivityForgotPasswordBinding
+
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+        // This is used to align the xml view to this class
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBar()
+
+        //TODO Step 2: Assign the click even to the submit button.
+        // In this screen there is only a one input field so we will not create the separate function what we have done in the Register and Login Screens.
+        // I will show you how to perform all the operations in the on click function it self.
+
+        // START
+        binding.btnSubmit.setOnClickListener {
+
+            // Get the email id from the input field.
+            val email: String = binding.etEmail.text.toString().trim { it <= ' ' }
+
+            // Now, If the email entered in blank then show the error message or else continue with the implemented feature.
+            if (email.isEmpty()) {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+            } else {
+
+                // Show the progress dialog.
+                showProgressDialog(resources.getString(R.string.please_wait))
+
+                // This piece of code is used to send the reset password link to the user's email id if the user is registered.
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+
+                        // Hide the progress dialog
+                        hideProgressDialog()
+
+                        if (task.isSuccessful) {
+                            // Show the toast message and finish the forgot password activity to go back to the login screen.
+                            Toast.makeText(
+                                this@ForgotPasswordActivity,
+                                resources.getString(R.string.email_sent_success),
+                                Toast.LENGTH_LONG
+                            ).show()
+
+                            finish()
+                        } else {
+                            showErrorSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    }
+            }
+        }
+        // END
+    }
+
+
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(binding.toolbarForgotPasswordActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        }
+
+        binding.toolbarForgotPasswordActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+}
--- app/src/main/java/com/pjff/mousywater/activities/LoginActivity.kt
+++ app/src/main/java/com/pjff/mousywater/activities/LoginActivity.kt
@@ -3,7 +3,10 @@ package com.pjff.mousywater.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.view.WindowManager
+import com.google.firebase.auth.FirebaseAuth
 import com.pjff.mousywater.R
 import com.pjff.mousywater.databinding.ActivityLoginBinding
 
@@ -13,7 +16,7 @@ import com.pjff.mousywater.databinding.ActivityLoginBinding
  * Login Screen of the application.
  */
 @Suppress("DEPRECATION")
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity(), View.OnClickListener {
     private lateinit var binding: ActivityLoginBinding
 
     /**
@@ -33,14 +36,91 @@ class LoginActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        // TODO Step 7: Assign a onclick event to the register text to launch the register activity.
         // START
-        binding.tvRegister.setOnClickListener {
-
-            // Launch the register screen when the user clicks on the text.
-            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
-            startActivity(intent)
-        }
+        // Click event assigned to Forgot Password text.
+        binding.tvForgotPassword.setOnClickListener(this)
+        // Click event assigned to Login button.
+        binding.btnLogin.setOnClickListener(this)
+        // Click event assigned to Register text.
+        binding.tvRegister.setOnClickListener(this)
         // END
+    }
+
+    /**
+     * In Login screen the clickable components are Login Button, ForgotPassword text and Register Text.
+     */
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+
+                R.id.tv_forgot_password -> {
+
+                    // Launch the forgot password screen when the user clicks on the forgot password text.
+                    val intent = Intent(this@LoginActivity, ForgotPasswordActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.btn_login -> {
+
+                    logInRegisteredUser()
+                }
+
+                R.id.tv_register -> {
+                    // Launch the register screen when the user clicks on the text.
+                    val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+
+    /**
+     * A function to validate the login entries of a user.
+     */
+    private fun validateLoginDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+            TextUtils.isEmpty(binding.etPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
+
+    /**
+     * A function to Log-In. The user will be able to log in using the registered email and password with Firebase Authentication.
+     */
+    private fun logInRegisteredUser() {
+
+        if (validateLoginDetails()) {
+
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            // Get the text from editText and trim the space
+            val email = binding.etEmail.text.toString().trim { it <= ' ' }
+            val password = binding.etPassword.text.toString().trim { it <= ' ' }
+
+            // Log-In using FirebaseAuth
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+
+                    // Hide the progress dialog
+                    hideProgressDialog()
+
+                    if (task.isSuccessful) {
+
+                        showErrorSnackBar("You are logged in successfully.", false)
+                    } else {
+                        showErrorSnackBar(task.exception!!.message.toString(), true)
+                    }
+                }
+        }
     }
 }
