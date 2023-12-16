ea4a4e311f96df0d3fe414c61dfa0a4d6d4b9611
parisollie <pjff18@gmail.com>
2023-12-15 19:55:17 -0600

    esqueleto fragments

---
+++ app/src/main/java/com/pjff/mousywater/ui/fragments/ProductsFragment.kt
@@ -0,0 +1,14 @@
+package com.pjff.mousywater.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+class ProductsFragment : Fragment() {
+
+
+}
