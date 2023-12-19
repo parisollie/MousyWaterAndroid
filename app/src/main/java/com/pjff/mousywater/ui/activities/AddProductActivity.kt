ccc91340743d1d62ecb99250e4cdbbd00d0e1266
parisollie <pjff18@gmail.com>
2023-12-19 15:42:26 -0600

    binding add products

--- app/src/main/java/com/pjff/mousywater/ui/activities/AddProductActivity.kt
+++ app/src/main/java/com/pjff/mousywater/ui/activities/AddProductActivity.kt
@@ -3,10 +3,18 @@ package com.pjff.mousywater.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pjff.mousywater.R
+import com.pjff.mousywater.databinding.ActivityAddProductBinding
+import com.pjff.mousywater.databinding.ActivitySettingsBinding
 
 class AddProductActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddProductBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_product)
+        // This is used to align the xml view to this class
+        binding = ActivityAddProductBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setupActionBar()
     }
 }
