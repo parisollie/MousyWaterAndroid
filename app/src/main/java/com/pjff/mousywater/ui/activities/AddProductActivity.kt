39a822b39ce54dfe129ee579360c3f8eeb77ece6
parisollie <pjff18@gmail.com>
2023-12-19 13:31:08 -0600

    Add products activity

---
+++ app/src/main/java/com/pjff/mousywater/ui/activities/AddProductActivity.kt
@@ -0,0 +1,12 @@
+package com.pjff.mousywater.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pjff.mousywater.R
+
+class AddProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_product)
+    }
+}
--- app/src/main/java/com/pjff/mousywater/ui/fragments/ProductsFragment.kt
+++ app/src/main/java/com/pjff/mousywater/ui/fragments/ProductsFragment.kt
@@ -1,60 +1,69 @@
 package com.pjff.mousywater.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.pjff.mousywater.R
-//Bueno
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-//Bueno
-/**
- * A simple [Fragment] subclass.
- * Use the [ProductsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ProductsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+import com.pjff.mousywater.ui.activities.AddProductActivity
+import com.pjff.mousywater.ui.activities.SettingsActivity
 
+class ProductsFragment : Fragment() {
+
+    //private lateinit var binding:
+
+    // TODO Step 5: Override the onCreate function and add the piece of code to inflate the option menu in fragment.
+    // START
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        // If we want to use the option menu in fragment we need to add it.
+        setHasOptionsMenu(true)
     }
+    // END
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_products, container, false)
+
+        val root = inflater.inflate(R.layout.fragment_products, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        textView.text = "This is dashboard Products"
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProductsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProductsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    // TODO Step 6: Override the onCreateOptionMenu function and inflate the Dashboard menu file init.
+    // START
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_product_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
+    // END
+
+    // TODO Step 7: Override the onOptionItemSelected function and handle the action items init.
+    // START
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        when (id) {
+
+            R.id.action_add_product -> {
+
+                // TODO Step 9: Launch the SettingActivity on click of action item.
+                // START
+                startActivity(Intent(activity, AddProductActivity::class.java))
+                // END
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    // END
 }
